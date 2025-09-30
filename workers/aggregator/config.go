c2e4b2a3c57a3d498b5254ef9d4ffd442f3056b6
ivanpfaab <pfaabivan@gmail.com>
2025-09-30 00:33:45 -0300

    feat: group by & aggregator & join skeleton addition

---
+++ workers/aggregator/config.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/tp-distribuidos-2c2025/shared/middleware"
+)
+
+const (
+	AggregatorExchangeName = "aggregator-exchange"
+	AggregatorRoutingKey   = "aggregator"
+	ReplyQueueName         = "orchestrator-reply-queue"
+	DefaultRabbitMQHost    = "localhost"
+	DefaultRabbitMQPort    = "5672"
+	DefaultRabbitMQUser    = "admin"
+	DefaultRabbitMQPass    = "password"
+)
+
+// loadConfig loads configuration from environment variables
+func loadConfig() (*middleware.ConnectionConfig, error) {
+	host := getEnv("RABBITMQ_HOST", DefaultRabbitMQHost)
+	port := getEnv("RABBITMQ_PORT", DefaultRabbitMQPort)
+	portInt, err := strconv.Atoi(port)
+	if err != nil {
+		return nil, fmt.Errorf("invalid port: %v", err)
+	}
+	username := getEnv("RABBITMQ_USER", DefaultRabbitMQUser)
+	password := getEnv("RABBITMQ_PASS", DefaultRabbitMQPass)
+
+	fmt.Printf("Aggregator Worker: Connecting to RabbitMQ at %s:%s with user %s\n", host, port, username)
+
+	return &middleware.ConnectionConfig{
+		Host:     host,
+		Port:     portInt,
+		Username: username,
+		Password: password,
+	}, nil
+}
+
+// getEnv gets an environment variable with a default value
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
