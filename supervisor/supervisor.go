eb2e17fe077d4e24e5a32954c5aca61be73a1a25
menda <jmendana@fi.uba.ar>
2025-11-27 01:19:04 -0300

    feat: added supervisor module (missing bully)

---
+++ supervisor/supervisor.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"log"
+
+	"github.com/tp-distribuidos-2c2025/shared/health_server"
+	"github.com/tp-distribuidos-2c2025/supervisor/docker_manager"
+	supervisor_election "github.com/tp-distribuidos-2c2025/supervisor/election"
+	"github.com/tp-distribuidos-2c2025/supervisor/monitor"
+)
+
+type Supervisor struct {
+	config        *SupervisorConfig
+	healthServer  *health_server.HealthServer
+	election      *supervisor_election.BullyElection
+	healthMonitor *monitor.HealthMonitor
+	dockerManager *docker_manager.DockerManager
+}
+
+func NewSupervisor(config *SupervisorConfig) (*Supervisor, error) {
+	healthServer := health_server.NewHealthServer("8888")
+
+	dockerManager := docker_manager.NewDockerManager(config.RestartTimeout)
+
+	if !dockerManager.IsDockerAvailable() {
+		log.Printf("[Supervisor] WARNING: Docker is not available. Container restarts will fail.")
+	}
+
+	supervisor := &Supervisor{
+		config:        config,
+		healthServer:  healthServer,
+		dockerManager: dockerManager,
+	}
+
+	bullyElection, err := supervisor_election.NewBullyElection(
+		config.SupervisorID,
+		config.Peers,
+		config.ElectionPort,
+		supervisor.onBecomeLeader,
+		supervisor.onLeaderChange,
+	)
+	if err != nil {
+		return nil, err
+	}
+	supervisor.election = bullyElection
+
+	return supervisor, nil
+}
+
+func (s *Supervisor) Start() error {
+	log.Printf("[Supervisor] Starting Supervisor %d", s.config.SupervisorID)
+
+	if err := s.healthServer.Start(); err != nil {
+		return err
+	}
+	log.Printf("[Supervisor] Health server started")
+
+	if err := s.election.Start(); err != nil {
+		return err
+	}
+	log.Printf("[Supervisor] Bully election started")
+
+	go s.election.StartElection()
+	log.Printf("[Supervisor] Initial election triggered")
+
+	return nil
+}
+
+func (s *Supervisor) onBecomeLeader() {
+	log.Printf("[Supervisor] Node %d: Became LEADER - starting health monitoring", s.config.SupervisorID)
+
+	if s.healthMonitor != nil {
+		s.healthMonitor.Stop()
+	}
+
+	s.healthMonitor = monitor.NewHealthMonitor(
+		s.config.MonitoredWorkers,
+		s.config.ProbeTimeout,
+		s.config.ProbeInterval,
+		s.restartWorker,
+	)
+
+	s.healthMonitor.Start()
+}
+
+func (s *Supervisor) onLeaderChange(newLeaderID int) {
+	log.Printf("[Supervisor] Node %d: New leader is %d - stopping health monitoring", s.config.SupervisorID, newLeaderID)
+
+	if s.healthMonitor != nil {
+		s.healthMonitor.Stop()
+		s.healthMonitor = nil
+	}
+}
+
+func (s *Supervisor) restartWorker(containerName string) {
+	log.Printf("[Supervisor] Restarting worker: %s", containerName)
+
+	err := s.dockerManager.RestartContainer(containerName)
+	if err != nil {
+		log.Printf("[Supervisor] Failed to restart %s: %v", containerName, err)
+	}
+}
+
+func (s *Supervisor) Stop() {
+	log.Printf("[Supervisor] Stopping Supervisor %d", s.config.SupervisorID)
+
+	if s.healthMonitor != nil {
+		s.healthMonitor.Stop()
+	}
+
+	if s.election != nil {
+		s.election.Stop()
+	}
+
+	if s.healthServer != nil {
+		s.healthServer.Stop()
+	}
+
+	log.Printf("[Supervisor] Stopped")
+}
+
+func (s *Supervisor) IsLeader() bool {
+	if s.election == nil {
+		return false
+	}
+	return s.election.IsLeader()
+}
